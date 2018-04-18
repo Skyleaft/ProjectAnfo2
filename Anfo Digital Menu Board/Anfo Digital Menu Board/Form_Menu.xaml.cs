77b55eb8166b6b625d342bda4f70e60f775e6e17
subhan4subhan <ridhosubhan03@gmail.com>
2018-04-18 15:57:30 +0700

    form menu beres ie mah

--- Anfo Digital Menu Board/Anfo Digital Menu Board/Form_Menu.xaml.cs
+++ Anfo Digital Menu Board/Anfo Digital Menu Board/Form_Menu.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MaterialDesignColors.WpfExample.Domain;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,76 @@ namespace Anfo_Digital_Menu_Board
         public Form_Menu()
         {
             InitializeComponent();
+            showdata();
+        }
+
+        koneksi k = new koneksi();
+        private String alamat_foto;
+
+        public void showdata()
+        {
+            k.sql = "select foto,id_produk,nama,harga from tb_produk";
+            k.setdt();
+            dg_menu.ItemsSource = k.dt.DefaultView;
+
+        }
+
+        public void bersih()
+        {
+            txt_harga.Text = "";
+            txt_kdmenu.Text = "";
+            txt_namamenu.Text = "";
+            img_foto.Source = null;
+        }
+
+        private void btn_ambil_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Masukan Foto";
+            op.Filter = "Semua Gambar|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                alamat_foto = op.FileName;
+                img_foto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void btn_simpan_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_kdmenu.Text=="" || txt_namamenu.Text == "" || txt_harga.Text == "")
+            {
+                var sampleMessageDialog = new SampleMessageDialog
+                {
+                    Message = { Text = "Lengkapi Dulu Data" }
+                };
+                DialogHost.Show(sampleMessageDialog, "MainDialog");
+            }
+            else
+            {
+                try
+                {
+                    k.sql = "insert into tb_produk select '" + txt_kdmenu.Text + "','" + txt_namamenu.Text + "','" + txt_harga.Text + "',bulkcolumn from openrowset(bulk'" + alamat_foto + "',single_blob) as gambar";
+                    k.setdt();
+
+                    var sampleMessageDialog = new SampleMessageDialog
+                    {
+                        Message = { Text = "Data Berhasil Tersimpan" }
+                    };
+                    DialogHost.Show(sampleMessageDialog, "MainDialog");
+                    showdata();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Data Gagal Didaftarkan " + ex);
+                }
+            }
+        }
+
+        private void btn_batal_Click(object sender, RoutedEventArgs e)
+        {
+            bersih();
         }
     }
 }
