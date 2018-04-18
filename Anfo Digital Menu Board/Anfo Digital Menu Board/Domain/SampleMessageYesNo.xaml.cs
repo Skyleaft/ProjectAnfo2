c515587da1236cdba15adb8e4f1a0d69162974fe
Milzan Malik <milzan_malik@yahoo.co.id>
2018-04-18 11:16:42 +0700

    initial library

---
+++ Anfo Digital Menu Board/Anfo Digital Menu Board/Domain/SampleMessageYesNo.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MaterialDesignThemes.Wpf;
+using ProjectPengolahanNilaiWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignColors.WpfExample.Domain
+{
+    /// <summary>
+    /// Interaction logic for SampleMessageDialog.xaml
+    /// </summary>
+    public partial class SampleMessageYesNo : UserControl
+    {
+        public SampleMessageYesNo()
+        {
+            InitializeComponent();
+        }
+        
+        private void btn_yes_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Anfo Digital Menu Board/Anfo Digital Menu Board/Koneksi/koneksi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anfo_Digital_Menu_Board
+{
+    class koneksi
+    {
+        public SqlConnection dbkoneksi;
+        public SqlCommand perintah;
+        public SqlDataAdapter da;
+        public DataTable dt;
+        public DataSet ds;
+        public String db = @"Data Source=192.168.1.117,1433;
+                            Network Library=DBMSSOCN;Initial Catalog=db_pointofsale;
+                            User ID=milzan;Password=12345678";
+        public String sql;
+
+        public void setdt()
+        {
+            dbkoneksi = new SqlConnection(db);
+            perintah = new SqlCommand(sql, dbkoneksi);
+            dt = new DataTable();
+            da = new SqlDataAdapter(perintah);
+            da.Fill(dt);
+        }
+
+        public void setds()
+        {
+            dbkoneksi = new SqlConnection(db);
+            perintah = new SqlCommand(sql, dbkoneksi);
+            ds = new DataSet();
+            da = new SqlDataAdapter(perintah);
+            da.Fill(ds, "tabel");
+
+        }
+
+        public void open()
+        {
+            dbkoneksi = new SqlConnection(db);
+            dbkoneksi.Open();
+        }
+        public void close()
+        {
+            dbkoneksi = new SqlConnection(db);
+            dbkoneksi.Close();
+        }
+    }
+}
+
