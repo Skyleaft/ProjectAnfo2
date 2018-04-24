3556325e53263938eeb5deada0c68b6d9df88fb8
Milzan Malik <milzan_malik@yahoo.co.id>
2018-04-24 16:35:14 +0700

    fix gambar added parameter koneksi

--- Anfo Digital Menu Board/Anfo Digital Menu Board/ByteImageConverter.cs
+++ Anfo Digital Menu Board/Anfo Digital Menu Board/ByteImageConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
-namespace POS_Application
+namespace Anfo_Digital_Menu_Board
 {
     public class ByteImageConverter
     {
@@ -28,13 +28,96 @@ namespace POS_Application
             return imgSrc;
         }
 
-        public static string ImageToByte(FileStream fs)
-        {
-            byte[] imgBytes = new byte[fs.Length];
-            fs.Read(imgBytes, 0, Convert.ToInt32(fs.Length));
-            string encodeData = Convert.ToBase64String(imgBytes, Base64FormattingOptions.InsertLineBreaks);
+        //public static string ImageToByte(FileStream fs)
+        //{
+        //    byte[] imgBytes = new byte[fs.Length];
+        //    fs.Read(imgBytes, 0, Convert.ToInt32(fs.Length));
+        //    string encodeData = Convert.ToBase64String(imgBytes, Base64FormattingOptions.InsertLineBreaks);
 
-            return encodeData;
+        //    return encodeData;
+        //}
+
+        public static byte[] ConvertBitmapSourceToByteArray(BitmapEncoder encoder, ImageSource imageSource)
+        {
+            byte[] bytes = null;
+            var bitmapSource = imageSource as BitmapSource;
+
+            if (bitmapSource != null)
+            {
+                encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+                using (var stream = new MemoryStream())
+                {
+                    encoder.Save(stream);
+                    bytes = stream.ToArray();
+                }
+            }
+
+            return bytes;
         }
+
+        public static byte[] ConvertBitmapSourceToByteArray(BitmapSource image)
+        {
+            byte[] data;
+            BitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+        public static byte[] ConvertBitmapSourceToByteArray(ImageSource imageSource)
+        {
+            var image = imageSource as BitmapSource;
+            byte[] data;
+            BitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+        public static byte[] ConvertBitmapSourceToByteArray(Uri uri)
+        {
+            var image = new BitmapImage(uri);
+            byte[] data;
+            BitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+        public static byte[] ConvertBitmapSourceToByteArray(string filepath)
+        {
+            var image = new BitmapImage(new Uri(filepath));
+            byte[] data;
+            BitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+
+        public static BitmapImage ConvertByteArrayToBitmapImage(Byte[] bytes)
+        {
+            var stream = new MemoryStream(bytes);
+            stream.Seek(0, SeekOrigin.Begin);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+
     }
 }
