de1eccf7449c09d3a09dbc287c5f08ecbcfde04f
Milzan Malik <milzan_malik@yahoo.co.id>
2018-05-31 10:34:26 +0700

    form menu dasboard

--- Anfo Digital Menu Board/Anfo Digital Menu Board/Views/MenuPage.xaml.cs
+++ Anfo Digital Menu Board/Anfo Digital Menu Board/Views/MenuPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Anfo_Digital_Menu_Board.Views
 {
@@ -20,9 +21,48 @@ namespace Anfo_Digital_Menu_Board.Views
     /// </summary>
     public partial class MenuPage : Page
     {
+
+        koneksi k = new koneksi();
         public MenuPage()
         {
             InitializeComponent();
+
+            lb_cpu.Text = HardwareInfo.GetProcessorname();
+            lb_ram.Text = HardwareInfo.GetPhysicalMemory();
+            lb_bios.Text = HardwareInfo.GetBIOSmaker();
+            lb_mobo.Text = HardwareInfo.GetBoardMaker();
+            lb_vga.Text = HardwareInfo.Getvganame();
+
+            
+
+            //timer-------------
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
+
+            timer.Interval = TimeSpan.FromMilliseconds(2500);
+            timer.Tick += timer_refreshData;
+            timer.Start();
+
+            //---------
+
         }
+
+        void timer_refreshData(object sender, EventArgs e)
+        {
+            k.sql = "select COUNT(id_produk) from tb_produk";
+            k.setdt();
+            tile_jmlprod.Count = k.dt.Rows[0][0].ToString();
+
+            k.sql = "select COUNT(id_katalog) from tb_katalog";
+            k.setdt();
+            tile_jmlktlog.Count = k.dt.Rows[0][0].ToString();
+
+            int l_WindowCount = System.Windows.Forms.Screen.AllScreens.Count();
+
+
+            tile_jmlmonitor.Count = l_WindowCount.ToString();
+
+
+        }
+
     }
 }
